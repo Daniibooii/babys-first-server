b5b68153574a023100673ae2c1a1c08d885bca15
unknown <gw-danmace@hotmail.com>
2018-02-28 13:58:33 -0500

    repo, folder, files created; code plugged in and tested

---
+++ server.js
@@ -0,0 +1,26 @@
+var http = require("http");
+
+var PORT = 7000;
+
+function handleRequest(request, response) {
+    response.end("You are great!: " + request.url);
+}
+
+var server = http.createServer(handleRequest);
+
+server.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
+
+var http = require("http");
+
+var PORT = 7500;
+
+function handleRequest(request, response) {
+    response.end("Thou art less than stellar! " + request.url);
+}
+
+var server = http.createServer(handleRequest);
+server.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
